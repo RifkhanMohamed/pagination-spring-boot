574d39feed5d396456dd1f0d9b191223ff74566c
Rifkhan94 <arrif@ou.ac.lk>
2021-05-05 13:41:29 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/modal/Student.java
@@ -0,0 +1,44 @@
+package com.example.demo.modal;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
